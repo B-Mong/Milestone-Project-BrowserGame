92ff63f1f393f9820fe1f945e607d9d7e4e084bd
Brian Mong <brian.mong9@gmail.com>
2022-06-13 21:53:17 -0700

    Created function to append items onto the DOM

--- game.js
+++ game.js
@@ -12,6 +12,26 @@ function gameRender(){
     tileMap.draw(canvas,ctx);
 }
 
+// Function that appends images to the DOM based on the X and Y coordinates
+function appendImage(url, left, bottom){
+    let image = document.createElement('img')
+    image.src = url
+    image.style.position = 'relative'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    image.style.zIndex = "1"
+    document.body.append(image)
+    return image
+}
+
 // Sets the refresh rate of the game so it can be rendered. Chose 60hz since that is the most common refresh rate
 setInterval(gameRender, 1000/60)
+
+
+// game canvas resolution 720 x 1072
+
+
+// adding objects to the game
+appendImage('assets/dungeonAssets/bigSilver.png', 680, 1070)
+appendImage('assets/dungeonAssets/silverBox.png', 698, 1070)
 
