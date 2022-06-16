719257b236ed6a4a7584d7ed800f3a1923c7c49d
Brian Mong <brian.mong9@gmail.com>
2022-06-16 01:58:09 -0700

    Added the popup for if the main function is true

--- scripts/endgame.js
+++ scripts/endgame.js
@@ -1,3 +1,22 @@
-var element = document.getElementById("inventory");
-var numberOfChildren = element.getElementsByTagName('*').length
-console.log(numberOfChildren)
+
+// Need code to check for if inventory == 6 then execute code below
+
+
+// Congratulates user
+let win = document.getElementById("attri");
+let update = document.createElement("p");
+update.textContent = 'You collected all the fruit!'
+win.appendChild(update)
+// Adds button to replay game
+let replay = document.createElement("button")
+replay.setAttribute("id","replayButton")
+replay.style
+replay.innerText = 'Replay!'
+replay.addEventListener("click",() =>{
+    location.reload()
+})
+win.appendChild(replay)
+
+// Checks how many fruit are inside of inventory
+let element = document.getElementById("inventory");
+let numberOfFruit = element.getElementsByTagName('*').length
